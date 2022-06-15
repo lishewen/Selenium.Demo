be9eb196be45b74e4929e3802e3dca4738383b3c
算神 <lishewen@msn.com>
2022-06-15 10:07:42 +0800

    添加项目文件。

---
+++ Selenium.Demo/Login.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium.Demo
+{
+    public static class LoginEx
+    {
+        public static bool Login(this EdgeDriver browser)
+        {
+            // 发起请求前先移除Chrome的window.navigator.webdriver参数，并随机等待，减少被检测的风险
+            IJavaScriptExecutor js = browser;
+            string returnjs = (string)js.ExecuteScript("Object.defineProperties(navigator, {webdriver:{get:()=>undefined}});");
+
+            browser.Navigate().GoToUrl("https://pc.xuexi.cn/points/my-points.html");
+            browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IWebElement qglogin = browser.FindElement(By.Id("qglogin"));
+            browser.ExecuteScript("arguments[0].scrollIntoView();", qglogin);
+
+            for (int i = 0; i < 60; i++)
+            {
+                if (browser.Url == "https://pc.xuexi.cn/points/my-points.html")
+                {
+                    Console.WriteLine("--> 登录成功");
+                    return true;
+                }
+                Thread.Sleep(5 * 1000);
+            }
+            return false;
+        }
+    }
+}
---
+++ Selenium.Demo/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using Selenium.Demo;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+Console.WriteLine("Hello, World!");
+
+new DriverManager().SetUpDriver(new EdgeConfig());
+
+var options = new EdgeOptions();
+// 防止检测
+options.AddExcludedArgument("enable-automation");
+options.AddAdditionalOption("useAutomationExtension", false);
+
+options.AddArgument("--mute-audio"); // 静音
+//options.AddAdditionalOption("excludeSwitches", new string[] { "enable-logging" }); // 禁止打印日志
+options.AddArgument("--ignore-certificate-errors"); // 忽略证书错误
+options.AddArgument("--ignore-ssl-errors"); // 忽略ssl错误
+options.AddArgument("–log-level=3");
+
+var driver = new EdgeDriver(options);
+
+// 窗口最大化
+driver.Manage().Window.Maximize();
+
+// 登录
+driver.Login();
+
+// Todo:操作
+
+// 关闭浏览器
+// driver.Quit();
